6056237f707865bee27dceace6f1d90db601534e
Robert Gregory <85454940+robertg1979@users.noreply.github.com>
2021-06-06 20:43:47 -0600

    Update test_password_hash.py
    Made changes to conftest.py and test file to remove extra boilerplate and make better use of test fixtures.

--- tests/functional_tests/test_password_hash.py
+++ tests/functional_tests/test_password_hash.py
@@ -1,8 +1,6 @@
 """
 Tests are written in PyTest. Please see the requirements.txt file for all dependencies needed.
 Note: PyTest tests can be included in single Python file, as shown here, or as separate files.
-Including this file as an example of non-BDD approach, but Gherkin-based frameworks are great for..
-..integration and end-to-end testing to this file may not be needed.
 Lastly, using mark to help with searching, tagging, etc., which is a great practice to follow.
 """
 from pytest import mark  # Using the mark function from PyTest to help group tests
@@ -14,115 +12,138 @@ from jsonschema import validate
 @mark.regression  # Include as part of regression tests
 @mark.password_hash  # Include as part of password_hash test suite
 # Creating post request test for hash endpoint
-def test_post_hash_endpoint(base_url, hash_endpoint, post_payload, headers):
-    url = base_url + hash_endpoint
-    # url = "http://127.0.0.1:8088/hash"
-    payload = post_payload
-    headers = headers
+def test_post_hash_endpoint(post_hash):
 
-    response = requests.request("POST", url, headers=headers, data=payload)  # Create variable to capture response
-    print(response.text)
-    assert response.status_code == 200
+    new_post = post_hash  # Make use of the test fixture that runs this post
+
+    print(new_post)
+
+    assert new_post.status_code == 200  # Basic 200 assertion here
+
+    assert int(new_post.text) >= 1  # The identifier returned should at least be one if successful
 
 
-# Negative test to check for invalid (NULL) body passed
 @mark.regression  # Include as part of regression tests
 @mark.password_hash  # Include as part of password_hash test suite
-def test_post_negative_missing_password(base_url, hash_endpoint, headers):
-    url = base_url + hash_endpoint
+# Negative test to check for invalid (NULL) body passed
+def test_post_negative_missing_password(host_url, hash_endpoint, headers):
 
-    payload = "{ \"password\":\"\"\r\n}"
+    url = host_url + hash_endpoint  # This time I am passing these through individually
+
+    payload = "{ \"password\":\"\"\r\n}"  # Sending a custom payload so not using the fixture for base request
+
     headers = headers
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data=payload)
+
     print(response.text)
+
     assert response.status_code == 400  # Should get 400 since no password is sent
 
 
+@mark.regression
+@mark.password_hash
 # Negative test to check for NO body passed
 # Should get 400 response
-@mark.regression  # Include as part of regression tests
-@mark.password_hash  # Include as part of password_hash test suite
-def test_post_negative_no_body(base_url, hash_endpoint, headers):
-    url = base_url + hash_endpoint
+def test_post_negative_no_body(host_url, hash_endpoint, headers):
 
-    payload = ""
+    url = host_url + hash_endpoint
+
+    payload = ""  # Passing NULL / empty payload
+
     headers = headers
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data=payload)
+
     print(response.text)
+
     assert response.status_code == 400  # Should get 400 since no password is sent
 
 
+@mark.smoke
+@mark.regression
+@mark.password_hash
 # Creating post request and then checking this is created with Get
-@mark.smoke  # Include as part of smoke tests
-@mark.regression  # Include as part of regression tests
-@mark.password_hash  # Include as part of password_hash test suite
-def test_post_end2end_hash(base_url, hash_endpoint, post_payload, headers):
+def test_post_end2end_hash(post_hash, host_url, hash_endpoint, headers):
     # Create a Post request to generate new identifier
-    url = base_url + hash_endpoint
-    # url = "http://127.0.0.1:8088/hash"
-    payload = post_payload
-    headers = headers
-    response = requests.request("POST", url, headers=headers, data=payload)  # Create variable to capture response
-    print(response.text)
+    new_post = post_hash  # Make use of the test fixture that runs this post
 
-    job_identifier = response.text  # Capture the response value as variable
+    job_identifier = new_post.text  # Capture the response value as a variable
     # Now send get request to check the latest identifier and retrieve hash result
-    url = base_url + hash_endpoint + '/' + job_identifier  # Use the base URL with /hash and new identifier
-    headers = headers
-    response = requests.request("GET", url, headers=headers)
+    url = host_url + hash_endpoint + '/' + str(job_identifier) # Use the base URL with /hash and new identifier
 
-    print(response.text)
-    assert response.status_code == 200
+    new_get = requests.get(url)  # Create response variable to capture result
+
+    print(new_get.text)  # Print out the value
+    assert new_get.status_code == 200  # Check for 200 response that confirms new POST request created a valid identifier
 
 
+@mark.smoke
+@mark.regression
+@mark.password_hash
 # Test a basic get call to hash for existing identifier
-@mark.smoke
+def test_get_functions_as_expected(get_hash):
+
+    new_get = get_hash  # Using the test fixture that uses 1 as the identifier by default
+
+    print(new_get.text)  # Print out the value
+
+    assert new_get.status_code == 200  # Check for 200 response
+    # Note: Following practice of Arrange, Act & Assert, so since one action here, more than one assertion is acceptable
+    assert len(new_get.text) == 88 # This is not a direct test of a base64 response, but does check if it has correct length
+
+
 @mark.regression
 @mark.password_hash
-def test_get_functions_as_expected(base_url, hash_endpoint, headers):
-
-    url = base_url + hash_endpoint + '/1'
-    headers = headers
-
-    response = requests.request("GET", url, headers=headers)
-
-    print(response.text)
-    assert response.status_code == 200
-
-
 # Test to validate that when sending a identifier that does not exist, 400 returned
-@mark.regression
-@mark.password_hash
-def test_get_invalid_identifier_check_400(base_url, hash_endpoint, headers):
+def test_get_invalid_identifier_check_400(host_url, hash_endpoint):
 
-    url = base_url + hash_endpoint + '/1000'
-    headers = headers
+    url = host_url + hash_endpoint + '/1000' # Just using a known value that will NOT exist
 
-    response = requests.request("GET", url, headers=headers)
+    new_get = requests.get(url)  # Create response variable to capture result
 
-    print(response.text)
-    assert response.status_code == 400  # Should get a 400 response in this scenario
+    print(new_get.text)
+    assert new_get.status_code == 400  # Should get a 400 response in this scenario
 
 
-# Test a basic get call to stats for 200 response
 @mark.smoke
 @mark.regression
 @mark.password_hash
-def test_get_stats_check_status_code_equals_200(base_url, stats_endpoint, schema):
-    url = base_url + stats_endpoint
+# Test a basic get call to stats for 200 response using schema
+def test_get_stats_check_status_code_equals_200(get_stats, schema):
 
-    response = requests.request("GET", url)
-
-    print(response.text)
+    new_get = get_stats
+    print(new_get.text)
 
     # Validate response headers and body contents, such as status code.
-    assert response.status_code == 200
+    assert new_get.status_code == 200
     # Validate response content type header is JSON
-    assert response.headers["Content-Type"] == "application/json"
+    assert new_get.headers["Content-Type"] == "application/json"
 
     # Example of validating against expected schema
-    data = requests.get(url).json()
+    data = new_get.json()
     validate(data, schema)
 
+
+@mark.smoke
+@mark.regression
+@mark.password_hash
+# Test that max number of requests is also available as a valid identifier
+# So this tests runs the get to stats and then checks the value against /hash as a get
+def test_max_requests_exists_as_identifier(get_stats, host_url, hash_endpoint):
+
+    new_get = get_stats  # Start with a get to stats
+
+    response_data = new_get.json()  # Capture the json body from results
+
+    print(response_data["TotalRequests"])  # Print out the value for for TotalRequests
+
+    total_request_val = response_data['TotalRequests']  # Capture the TotalRequests value as variable
+
+    url = host_url + hash_endpoint + '/' + str(total_request_val) # Use the base URL with /hash and new identifier
+
+    new_get = requests.get(url)  # Create response variable to capture result
+
+    print(new_get.text)
+
+    assert new_get.status_code == 200  # Check that this identifier exists
